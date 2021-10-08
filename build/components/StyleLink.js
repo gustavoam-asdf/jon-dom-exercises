5976d4d107ae387f15ed1563ebe7ada0173add0a
Gustavo A <atencio361@hotmail.com>
2021-10-08 17:19:54 -0500

    Updates

--- build/components/StyleLink.js
+++
@@ -1,7 +0,0 @@
-var StyleLink = function (path) {
-    var link = document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', "./styles/" + path + ".css");
-    return link;
-};
-export default StyleLink;
