48625ad3bd90f5cb0ce8283a19f16b8ac8d3c29c
Gustavo A <atencio361@hotmail.com>
2021-10-08 17:49:36 -0500

    Using font awesome

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,10 @@
 import Header from './components/Header'
 import Menu from './components/Menu'
 import 'normalize.css'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 const App = (): DocumentFragment => {
   const app: DocumentFragment = document.createDocumentFragment()
