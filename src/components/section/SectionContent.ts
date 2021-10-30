b63d64bd598da3f42f3034798c1d54d47789e95d
Gustavo A <gustavo.atencio@outlook.com>
2021-10-29 22:27:48 -0500

    Adding template string on section

--- src/components/section/SectionContent.ts
+++ src/components/section/SectionContent.ts
@@ -1,13 +1,17 @@
 const SectionContentTemplate = ({
   children
 }: {
-  children: HTMLElement[] | HTMLElement
+  children: HTMLElement[] | HTMLElement | string
 }): DocumentFragment => {
   const content = document.createDocumentFragment()
   if (Array.isArray(children)) {
     children.forEach(child => content.appendChild(child))
-  } else if (children) {
+  } else if (children instanceof HTMLElement) {
     content.appendChild(children)
+  } else {
+    const tmp = document.createElement('div')
+    tmp.outerHTML = children
+    content.appendChild(tmp)
   }
 
   return content
@@ -15,8 +19,12 @@ const SectionContentTemplate = ({
 
 export default class SectionContent {
   public self: DocumentFragment
-  public children: HTMLElement[] | HTMLElement
-  constructor({ children = [] }: { children?: HTMLElement[] | HTMLElement }) {
+  public children: HTMLElement[] | HTMLElement | string
+  constructor({
+    children = []
+  }: {
+    children?: HTMLElement[] | HTMLElement | string
+  }) {
     this.children = children
     this.self = SectionContentTemplate({ children: this.children })
   }
