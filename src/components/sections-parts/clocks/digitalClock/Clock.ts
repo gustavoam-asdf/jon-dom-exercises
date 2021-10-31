c06c81accdd9e88b7cbcbfe1c3e42e45eb6b05bc
Gustavo A <gustavo.atencio@outlook.com>
2021-10-30 23:38:09 -0500

    Soporting 12 hours time

--- src/components/sections-parts/clocks/digitalClock/Clock.ts
+++ src/components/sections-parts/clocks/digitalClock/Clock.ts
@@ -3,31 +3,44 @@ interface Time {
   minutes: string
   seconds: string
   separator: string
+  format?: string
 }
 
-const clockParts = (time: string): Time => {
-  if (time.length !== 8) {
-    throw new Error("Invalid time format")
+const clockParts = (localTime: string): Time => {
+  let usedTime = localTime
+  let currFormat = ""
+  const hour12: boolean = /(a|p)m/i.test(localTime)
+  if (hour12) {
+    const [time, format] = localTime.split(/\s/i)
+    usedTime = time
+    currFormat = format
   }
-  const [hours, minutes, seconds] = time.split(":")
-  return {
+  const [hours, minutes, seconds] = usedTime.split(":")
+  const time: Time = {
     hours: `<span class="${Clock.className}-part hours">${hours}</span>`,
     minutes: `<span class="${Clock.className}-part minutes">${minutes}</span>`,
     seconds: `<span class="${Clock.className}-part seconds">${seconds}</span>`,
     separator: `<span class="${Clock.className}-part separator">:</span>`
   }
+  if (hour12) {
+    time.format = `<span class="${Clock.className}-part format">${currFormat}</span>`
+  }
+  return time
 }
 
 const ClockTemplate = ({
   hours,
   minutes,
   seconds,
-  separator
+  separator,
+  format
 }: Time): HTMLDivElement => {
   const clock: HTMLDivElement = document.createElement("div")
   clock.classList.add(Clock.className)
   clock.classList.add("hide")
-  clock.innerHTML = `${hours}${separator}${minutes}${separator}${seconds}`
+  clock.innerHTML = `${hours}${separator}${minutes}${separator}${seconds}${
+    format ? format : ""
+  }`
   return clock
 }
 
