f011654b1bbfa8c240a9c61c6e95d7b70a16e4ba
Gustavo A <gustavo.atencio@outlook.com>
2021-11-13 01:39:06 -0500

    Using common typescript declaration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,5 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-non-null-assertion": "off"
   },
-  ignorePatterns: ["build/", "webpack.config.js", "declarations"]
+  ignorePatterns: ["build/", "webpack.config.js", "types"]
 }
