810034789196edc76ba0672a9b80792f1c988fd9
Gustavo A <gustavo.atencio@outlook.com>
2021-11-28 10:46:19 -0500

    Renaming css files parts

--- src/components/sections-parts/countdown/countdownPart.ts
+++ src/components/sections-parts/countdown/countdownPart.ts
@@ -1,6 +1,6 @@
 import SectionContent from "../../section/SectionContent"
 import Countdown from "./Countdown"
-import "@styles/components/countdown/countdown.css"
+import "@styles/components/countdown/countdownPart.css"
 
 export default new SectionContent({
   children: new Countdown()
