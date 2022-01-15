b011af3a94a257ce62abd3270d794446078a4cf2
Gustavo A <gustavo.atencio@outlook.com>
2022-01-15 10:54:04 -0500

    Private icon change

--- src/components/Panel/ThemeControl/index.ts
+++ src/components/Panel/ThemeControl/index.ts
@@ -8,13 +8,15 @@ const icons = { sun: "fa-sun", moon: "fa-moon" }
 export default class ThemeControl {
   public self: PageControl
   public icon: Icon
-  public initialIcon: string
+  private initialIcon: string
+  private targetIcon: string
   public themeData: ThemeData
   static className = "theme-button"
 
   constructor() {
     this.themeData = detectTheme()
     this.initialIcon = this.themeData.isDark ? icons.sun : icons.moon
+    this.targetIcon = this.themeData.isDark ? icons.moon : icons.sun
     this.icon = new Icon({
       iconType: this.initialIcon,
       className: `${ThemeControl.className}__icon`
@@ -25,9 +27,7 @@ export default class ThemeControl {
 
   action() {
     this.themeData = switchTheme(this.themeData)
-    this.icon.switchIcon(
-      this.initialIcon === icons.sun ? icons.moon : icons.sun
-    )
+    this.icon.switchIcon(this.targetIcon)
     saveTheme(this.themeData)
   }
 }
