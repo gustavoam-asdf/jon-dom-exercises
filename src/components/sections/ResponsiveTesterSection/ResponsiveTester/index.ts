2ec755cdfe8e9927ac6268ae0c93abd42ab5c5a5
Gustavo A <gustavo.atencio@outlook.com>
2022-01-22 12:22:10 -0500

    Click event for open and close window tested

--- src/components/sections/ResponsiveTesterSection/ResponsiveTester/index.ts
+++ src/components/sections/ResponsiveTesterSection/ResponsiveTester/index.ts
@@ -1,15 +1,54 @@
 import Button from "@components/Button"
+import executeClick from "@utils/executeClick"
 import { $ } from "@utils/selector"
 
 export default class ResponsiveTester implements SectionChild {
   public self: HTMLDivElement
+  public button: Button
+  public window?: Window
   static className = "responsive-tester"
 
   constructor() {
+    this.button = new Button({ type: "submit", innerHTML: "Testear" })
     this.self = this.template()
     setTimeout(() => {
       $(document.documentElement, '[href="#responsive-tester"]').click()
     }, 1000)
+  }
+
+  public clickEvent(evt: any) {
+    return executeClick({
+      evt,
+      selectors: `.${ResponsiveTester.className} button`,
+      action: () => {
+        evt.preventDefault()
+        if (this.window) {
+          this.window.close()
+          this.window = undefined
+          this.button.textContent = "Testear"
+          return
+        }
+
+        const url = $<HTMLInputElement>(this.self, "input[name='url']").value
+        const pageWidth = $<HTMLInputElement>(
+          this.self,
+          "input[name='pageWidth']"
+        ).value
+        const pageHeight = $<HTMLInputElement>(
+          this.self,
+          "input[name='pageHeight']"
+        ).value
+
+        this.window = window.open(
+          url,
+          "",
+          `popup, width=${pageWidth}, height=${pageHeight}`
+        ) as Window
+        this.button.textContent = "Cerrar"
+
+        console.log({ url, pageWidth, pageHeight })
+      }
+    })
   }
 
   private template() {
@@ -17,9 +56,9 @@ export default class ResponsiveTester implements SectionChild {
     nestedWeb.classList.add(ResponsiveTester.className)
 
     nestedWeb.innerHTML = `
-      <form name="form">
+      <form>
         <label>
-          <span>Url:</span>
+          <span>Url :</span>
           <input 
             type="text" 
             id="url" 
@@ -28,7 +67,7 @@ export default class ResponsiveTester implements SectionChild {
           >
         </label>
         <label>
-          <span>Ancho:</span>
+          <span>Ancho :</span>
           <input 
             type="text" 
             id="pageWidth"  
@@ -37,7 +76,7 @@ export default class ResponsiveTester implements SectionChild {
           >
         </label>
         <label>
-          <span>Largo:</span>
+          <span>Largo :</span>
           <input 
             type="text" 
             id="pageHeight"
@@ -48,9 +87,7 @@ export default class ResponsiveTester implements SectionChild {
       </form>
     `
 
-    $(nestedWeb, "form").append(
-      new Button({ type: "submit", innerHTML: "Testear" })
-    )
+    $(nestedWeb, "form").append(this.button)
 
     return nestedWeb
   }
--- src/utils/selector.ts
+++ src/utils/selector.ts
@@ -1,5 +1,7 @@
-export const $ = (container: HTMLElement, selector: string) =>
-  container.querySelector(selector) as HTMLElement
+export const $ = <T extends HTMLElement>(
+  container: HTMLElement,
+  selector: string
+) => container.querySelector(selector) as T
 
 export const $$ = (container: HTMLElement, selector: string) =>
   container.querySelectorAll(selector) as NodeListOf<HTMLElement>
